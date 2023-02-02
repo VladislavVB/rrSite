a6e5aec57cccec7fd8d8c1cc806bd6b247224711
vladislav <vladislav.b@affetta.ru>
2023-02-02 14:44:17 +0300

    callendar

---
+++ src/scripts/afisha.js
@@ -0,0 +1,24 @@
+
+const calendar = new VanillaCalendar('#calendar', {
+  settings: {
+    lang: 'ru',
+    range: {
+      min: '2023-01-01',
+      max: '2023-03-01',
+    },
+    selected: {
+      holidays: ['2023-02-03', '2023-02-10', '2023-02-20'],
+    },
+    visibility: {
+      today: true,
+    },
+
+  },
+  actions: {
+		clickDay(event, dates) {
+      console.log({event},{dates});
+    },
+	},
+ 
+});
+calendar.init();
